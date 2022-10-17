1d3e6268508544f6ffbcc88cf3b268eae35690ef
mkvasconcelos <mateuskvasconcelos@gmail.com>
2022-10-17 17:49:18 -0300

    Wrong choices are corrected.

--- js/main.js
+++ js/main.js
@@ -21,30 +21,41 @@ function shuffle(array){
 choice = shuffle(choice);
 const main = document.querySelector('main');
 for (let i = 0; i < cards; i++){
-    // main.querySelectorAll(':scope > button')[i].classList.add("cards");
     main.children[i].classList.add("cards");
     main.children[i].innerHTML = choice[i];
 };
 let clicks = 0;
 let card_choice = ""; 
 let card_button = "";
+let gotcha = false;
+
 function click_choice(value){
     value.classList.toggle(value.innerHTML);
-    console.log(clicks, card_button, card_choice);
     if (clicks === 0){
         card_choice = value.innerHTML;
         card_button = value;
         clicks++;
+        console.log("Passo 1: ", clicks, card_button, card_choice);
+        return
+    } else if (card_button === value){
+        console.log("Passo 2: ",clicks, card_button, card_choice);
         return
     } else if (card_choice === value.innerHTML){
         card_button.classList.add("card_gotcha");
         value.classList.add("card_gotcha");
+        card_button.disabled = true;
+        value.disabled = true;
+        console.log("Passo 3: ",clicks, card_button, card_choice);
     } else{
-        card_button.classList.toggle(value.innerHTML);
-        value.classList.toggle(value.innerHTML);
+        setTimeout(wrong_choice, 2000, value, card_button);
     }
     clicks = 0;
     card_choice = "";
     card_button = "";
     return
+}
+
+function wrong_choice(card1,card2){
+    card1.classList.toggle(card1.innerHTML);
+    card2.classList.toggle(card2.innerHTML);
 }
